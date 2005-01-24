4c2fc7f728fda5405e214fca0a827bb50e8f30cb
xzhan27 <xzhan27@5428276e-be0b-f542-9301-ee418ed919ad>
2005-01-24 04:52:45 +0000

    add more filters for cygwin support

--- trunk/cyg.sh
+++ trunk/cyg.sh
@@ -2,7 +2,7 @@
 
 
 
-sed -e "s/printf/_printf/g" | sed -e "s/malloc/_malloc/g" | \
+sed -e "s/printf/_printf/g" | sed -e "s/malloc/_malloc/g" | sed -e "s/getchar/_getchar/g" | sed -e "s/free/_free/g" | sed -e "s/realloc/_realloc/g" | sed -e "s/atoi/_atoi/g" | sed -e "s/strcmp/_strcmp/g" | \
 
-sed -e "s/exit/_exit/g" | sed -e "s/\.comm _exception_value,4,4/\.comm _exception_value,4/g" | sed -e "s/\.comm _exception_type,4,4/\.comm _exception_type,4/g" | sed -e "s/\.comm _exception_dimension,4,4/\.comm _exception_value,4/g" | sed -e "/\.type/ d" | sed -e "s/main/_main/"
+sed -e "s/exit/_exit/g" | sed -e "s/\.comm _exception_value,4,4/\.comm _exception_value,4/g" | sed -e "s/\.comm _exception_type,4,4/\.comm _exception_type,4/g" | sed -e "s/\.comm _exception_dimension,4,4/\.comm _exception_value,4/g" | sed -e "/\.type/ d" #| sed -e "s/main/_main/"
 
