8346df5e6c707761409fa7d2e0669b6cf892116e
nobody <nobody@5428276e-be0b-f542-9301-ee418ed919ad>
2006-02-06 12:21:17 +0000

    This commit was manufactured by cvs2svn to create branch 'vendor'.

---
+++ branches/vendor/Resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TopTool.rc
+//
+#define IDR_MANIFEST                    1
+#define IDD_TOPTOOL_DIALOG              102
+#define IDR_POPUP_MENU                  129
+#define IDR_ICON                        129
+#define ID__130                         130
+#define IDM_QUIT                        131
+#define IDC_STATIC_MAILME               1000
+#define IDC_STATIC_MAILME2              1001
+#define IDM_ABOUT                       57664
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
