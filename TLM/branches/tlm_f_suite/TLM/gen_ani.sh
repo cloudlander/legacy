6f8f28db7c9f062b8f4ddaf0b51f4980bebf5608
xmzhang <xmzhang@5428276e-be0b-f542-9301-ee418ed919ad>
2008-02-19 02:45:17 +0000

    changes for linux

---
+++ TLM/branches/tlm_f_suite/TLM/gen_ani.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd EY_MAP
+#mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../map_truecolor.avi
+mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../map_color.avi
+
+cd ../EY_3D
+#mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../3d_truecolor.avi
+mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../3d_color.avi
