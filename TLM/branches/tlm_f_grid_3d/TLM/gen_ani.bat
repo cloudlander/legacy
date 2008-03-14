6e00ccc1bedf92d1057b1a609b12d00610e7a674
xmzhang <xmzhang@5428276e-be0b-f542-9301-ee418ed919ad>
2008-03-14 07:57:41 +0000

    animation generation scripts updated to support 3D-TLM

--- TLM/branches/tlm_f_grid_3d/TLM/gen_ani.bat
+++ TLM/branches/tlm_f_grid_3d/TLM/gen_ani.bat
@@ -1,7 +1,10 @@
-cd EY_MAP
-..\mencoder\mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ..\map_truecolor.avi
-..\mencoder\mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ..\map_color.avi
+for /D %%v in (EX EY EZ HX HY HZ) do call :gen %%v
+goto :end
 
-cd ..\EY_3D
-..\mencoder\mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ..\3d_truecolor.avi
-..\mencoder\mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ..\3d_color.avi
+:gen
+cd %1_MAP
+start "%1_MAP" /WAIT ..\mencoder\mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ..\%1_map_color.avi
+cd ..\%1_3D
+start "%1_3D" /WAIT ..\mencoder\mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ..\%1_3d_color.avi
+cd ..
+:end
--- TLM/branches/tlm_f_grid_3d/TLM/gen_ani.sh
+++ TLM/branches/tlm_f_grid_3d/TLM/gen_ani.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-cd EY_MAP
-#mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../map_truecolor.avi
-mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../map_color.avi
-
-cd ../EY_3D
-#mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../3d_truecolor.avi
-mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../3d_color.avi
+for dir in EX EY EZ HX HY HZ
+do
+  cd ${dir}_MAP
+  mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../${dir}_map_color.avi
+  cd ../${dir}_3D
+  mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../${dir}_3d_color.avi
+  cd ..
+done
