1a6ceb9537313668da0b61d90d431b4a01a6de3c
xmzhang <xmzhang@5428276e-be0b-f542-9301-ee418ed919ad>
2009-03-03 18:00:14 +0000

    replaced by tlm_f_grid, which is integrated with Gaussian and TLM.py support.

--- TLM/branches/tlm_f_grid_gauss/TLM/gen_ani.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd EY_MAP
-#mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../map_truecolor.avi
-mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../map_color.avi
-
-cd ../EY_3D
-#mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf palette,scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../3d_truecolor.avi
-mencoder mf://*.png -mf w=1024:h=768:fps=25:type=png -vf scale -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -o ../3d_color.avi
